ae21d0a276d5602d02735cae1bee20bc72e56ad6
Zhijie Nie <nie@ieee.org>
2019-02-07 16:54:41 -0800

    Ready to develop functions of constraints

--- opf.py
+++ opf.py
@@ -1,8 +1,7 @@
 from case import Case, Const
 import numpy as np
 from scipy.optimize import *
-# from scipy.optimize import Bounds, LinearConstraint, NonlinearConstraint
-import pdb
+from pdb import *
 
 
 def runcopf(c):
@@ -35,27 +34,42 @@ def runcopf(c):
 
     xmin[(c.bus.take(const.BUS_TYPE, axis=1) == 3).nonzero()] = 0
     xmax[(c.bus.take(const.BUS_TYPE, axis=1) == 3).nonzero()] = 0
-    
-    f = costfcn(x0,c)
 
-    bounds = Bounds(xmin, xmax)
+    ####################################################################
+    # Polynomial Cost Functions (f)
+    #################################################################### 
+    f_fcn   = lambda x: costfcn(x, c)
+    df_fcn  = lambda x: costfcn_jac(x, c)
+    d2f_fcn = lambda x: costfcn_hess(x, c)
 
+    ####################################################################
+    # Simple Power Balance Constraint (Linear, lossless)
+    ####################################################################   
     simple_powerbalance = np.zeros(x0.shape[0])
     tload = sum(c.bus[:,const.PD]) / c.mva_base
     simple_powerbalance[ii['i1']['pg']:ii['iN']['pg']] = 1
     simple_lincons = LinearConstraint(simple_powerbalance, tload, tload)
+    # simple_lincons = {'type': 'eq',
+    #                   'fun': lambda x : sum(x[ii['i1']['pg']:ii['iN']['pg']]) - tload}
+    
+    ####################################################################
+    # Nonlinear Power Flow Constraints (g: eqcons, h: ineqcons)
+    ####################################################################   
+    # eqcons = NonlinearConstraint()
+    # ineqcons = NonlinearConstraint()
+    # eqcons = {'type': 'eq',
+    #           'fun' : lambda x: eqconsfcn(x, c),
+    #           'jac' : lambda x: eqconsfcn_jac(x,c)}
+              
+    # ineqcons = {'type': 'eq',
+    #             'fun' : lambda x: ineqconsfcn(x, c),
+    #             'jac' : lambda x: ineqconsfcn_jac(x,c)}
 
-    pdb.set_trace()
 
-    f_fcn = lambda x: costfcn(x, c)
+    res = minimize(f_fcn, x0, jac=df_fcn, hess=d2f_fcn, \
+                   constraints=[simple_lincons], bounds=Bounds(xmin, xmax))
 
-    res = minimize(f_fcn, x0, constraints=[simple_lincons], bounds=bounds)
-
-    print(tload)
-    print(res.x[ii['i1']['pg']:ii['iN']['pg']])
-    print(sum(res.x[ii['i1']['pg']:ii['iN']['pg']]))
-
-
+    print(res)
     
 
 def costfcn(x, c):
@@ -68,8 +82,40 @@ def costfcn(x, c):
     gencost = np.zeros(ng)
     for gi in range(ng):
         gencost[gi] = polycost(c.gencost[gi], pg[gi])
-    f = gencost.sum()
-    return f
+
+    return gencost.sum()
+
+def costfcn_jac(x, c):
+    ng = c.gen.shape[0]
+    ii = get_var_idx(c)
+
+    pg = c.mva_base * x[ii['i1']['pg']:ii['iN']['pg']]
+    qg = c.mva_base * x[ii['i1']['qg']:ii['iN']['qg']]
+
+    gencost_jac = np.zeros(ng)
+    for gi in range(ng):
+        gencost_jac[gi] = c.mva_base * polycost_jac(c.gencost[gi], pg[gi])
+
+    df = np.zeros(x.shape[0])
+    df[ii['i1']['pg']:ii['iN']['pg']] = gencost_jac
+
+    return df
+
+def costfcn_hess(x, c):
+    ng = c.gen.shape[0]
+    ii = get_var_idx(c)
+
+    pg = c.mva_base * x[ii['i1']['pg']:ii['iN']['pg']]
+    qg = c.mva_base * x[ii['i1']['qg']:ii['iN']['qg']]
+
+    gencost_hess = np.zeros(ng)
+    for gi in range(ng):
+        gencost_hess[gi] = c.mva_base ** 2 * polycost_hess(c.gencost[gi], pg[gi])
+
+    d2f = np.zeros((x.shape[0], x.shape[0]))
+    d2f[ii['i1']['pg']:ii['iN']['pg'], ii['i1']['pg']:ii['iN']['pg']] = np.diag(gencost_hess)
+
+    return d2f
 
 def polycost(cost_metrics, pg):
     const = Const()
@@ -77,6 +123,25 @@ def polycost(cost_metrics, pg):
     pn = int(cost_metrics[const.NCOST])
     for pi in range(pn):
         cost += cost_metrics[-(1+pi)] * pg ** pi
+    
+    return cost
+
+def polycost_jac(cost_metrics, pg):
+    const = Const()
+    cost = 0.
+    pn = int(cost_metrics[const.NCOST])
+    for pi in range(1, pn):
+        cost += pi * cost_metrics[-(1+pi)] * pg ** (pi - 1)
+
+    return cost
+
+def polycost_hess(cost_metrics, pg):
+    const = Const()
+    cost = 0.
+    pn = int(cost_metrics[const.NCOST])
+    for pi in range(2, pn):
+        cost += pi * cost_metrics[-(1+pi)] * pg ** (pi - 2)
+
     return cost
 
 def get_var_idx(c):
